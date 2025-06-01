c481898de9c59afcc9bdf13f1fc5e3fc3ae71fc9
Victor  Balan <victor.balan@assist.ro>
2025-06-01 20:18:50 +0300

    Initial setup

---
+++ src/test/java/com/vbalan/rate_limiter/RateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbalan.rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
